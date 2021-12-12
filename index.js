45d872385c8fe581af5f29ea4deec8a5744a5696
nonymus432 <95450860+nonymus432@users.noreply.github.com>
2021-12-12 04:59:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,16 @@ var hours = today.getHours() + " AM";
 
 
 document.getElementById("today").innerHTML = today.toUTCString();
-document.getElementById("day").innerHTML = "Today is " + daylist[day] + '. Which is the ' + day + ' of the 7 days in a week.';
+document.getElementById("today").innerHTML = today.toUTCString();
+if (day === 0){
+ document.getElementById("day").innerHTML = "Today is " + daylist[day] + '. Which is the ' + 7 + ' of the 7 days in a week.';
+} else {
+ document.getElementById("day").innerHTML = "Today is " + daylist[day] + '. Which is the ' + day + ' of the 7 days in a week.';
+}
+
+
+
+//document.getElementById("day").innerHTML = "Today is " + daylist[day] + '. Which is the ' + day + ' of the 7 days in a week.';
 document.getElementById("MonthDay").innerHTML = "Which is the day " + MonthDay +" of " + monthList[month];
 document.getElementById("time").innerHTML = "Current time is : " + hours + " : " + minutes + " : " + seconds;
 console.log(today);
